acc20066cab215b0e6f17fae0d0fec7c1a9a3f08
Dmitry Gordeev <dmitrijg934@gmail.com>
2024-07-13 20:08:37 +0300

    Application skeleton ready

--- src/nn/dgordeev/jdbc/starter/JdbcRunner.java
+++ src/nn/dgordeev/jdbc/starter/JdbcRunner.java
@@ -1,8 +1,16 @@
 package nn.dgordeev.jdbc.starter;
 
+import nn.dgordeev.jdbc.starter.util.ConnectionManager;
+
+import java.sql.SQLException;
+
 public class JdbcRunner {
 
     public static void main(String[] args) {
-
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/nn/dgordeev/jdbc/starter/util/ConnectionManager.java
@@ -0,0 +1,56 @@
+package nn.dgordeev.jdbc.starter.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static nn.dgordeev.jdbc.starter.util.ConnectionManager.PropertyKey.DB_URL;
+import static nn.dgordeev.jdbc.starter.util.ConnectionManager.PropertyKey.PASSWORD;
+import static nn.dgordeev.jdbc.starter.util.ConnectionManager.PropertyKey.USERNAME;
+
+public final class ConnectionManager {
+
+    static {
+        loadDriver();
+    }
+
+    private ConnectionManager() {
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(DB_URL),
+                    PropertiesUtil.get(USERNAME),
+                    PropertiesUtil.get(PASSWORD)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public enum PropertyKey {
+
+        DB_URL("db.url"),
+        USERNAME("db.username"),
+        PASSWORD("db.password");
+
+        private final String value;
+
+        PropertyKey(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
