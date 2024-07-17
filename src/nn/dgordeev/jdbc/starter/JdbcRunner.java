74dc068bc380252e23de70f0a6a93065b210e3d4
Dmitry Gordeev <dmitrijg934@gmail.com>
2024-07-17 18:23:07 +0300

    Fetch size property added

--- src/nn/dgordeev/jdbc/starter/JdbcRunner.java
+++ src/nn/dgordeev/jdbc/starter/JdbcRunner.java
@@ -1,10 +1,14 @@
 package nn.dgordeev.jdbc.starter;
 
 import nn.dgordeev.jdbc.starter.util.ConnectionManager;
+import nn.dgordeev.jdbc.starter.util.PropertiesUtil;
 
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashSet;
+
+import static java.util.Optional.ofNullable;
+import static nn.dgordeev.jdbc.starter.util.ConnectionManager.PropertyKey.FETCH_SIZE;
 
 public class JdbcRunner {
 
@@ -19,14 +23,18 @@ public class JdbcRunner {
 
     private static void checkMetaData() throws SQLException {
         try (var connection = ConnectionManager.get()) {
+            int fetchSize = getFetchSize();
             var metaData = connection.getMetaData();
             var catalogs = metaData.getCatalogs();
+            catalogs.setFetchSize(fetchSize);
             while (catalogs.next()) {
                 var catalog = catalogs.getString(1);
                 var schemas = metaData.getSchemas();
+                schemas.setFetchSize(fetchSize);
                 while (schemas.next()) {
                     var schema = schemas.getString("TABLE_SCHEM");
                     var tables = metaData.getTables(catalog, schema, "%", new String[]{"TABLE"});
+                    tables.setFetchSize(fetchSize);
                     if (schema.equals("bookings")) {
                         while (tables.next()) {
                             System.out.println(tables.getString("TABLE_NAME"));
@@ -35,6 +43,12 @@ public class JdbcRunner {
                 }
             }
         }
+    }
+
+    private static int getFetchSize() {
+        return ofNullable(PropertiesUtil.get(FETCH_SIZE))
+                .map(Integer::parseInt)
+                .orElse(5);
     }
 
     private static Collection<String> getAircraftCodesByRange(int startRange, int endRange) {
